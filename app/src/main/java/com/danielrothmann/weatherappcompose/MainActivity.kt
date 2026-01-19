5e55678e1f347fe623ac96a5e388e5f7cd97cc80
Daniel Rotman <danilarothmann@gmail.com>
2026-01-19 10:47:46 +0700

    main Screen

--- app/src/main/java/com/danielrothmann/weatherappcompose/MainActivity.kt
+++ app/src/main/java/com/danielrothmann/weatherappcompose/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.danielrothmann.weatherappcompose.ui.theme.WeatherAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +31,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherAppComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    WeatherInfoCard(
+                        name = "Sity",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +42,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun WeatherInfoCard (name: String, modifier: Modifier = Modifier) {
+    val state = remember{
+        mutableStateOf("Unknow")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Верхняя часть - погода
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color.LightGray),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Temp in $name! ${state.value} C",
+                fontSize = 24.sp
+            )
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherAppComposeTheme {
-        Greeting("Android")
+        // Нижняя часть - кнопка
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /* Обновить погоду */ }
+            ) {
+                Text(
+                    text = "Refresh",
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
+            }
+        }
     }
 }
